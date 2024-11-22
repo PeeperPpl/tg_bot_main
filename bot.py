fee3d4eb796e69f7ada6137fb9985dc1a9d0ab6f
RashingPro <rashingpro@yandex.ru>
2024-11-22 07:29:26 +0300

    Add sample to test menu feature

--- bot.py
+++ bot.py
@@ -13,13 +13,21 @@ bot = AsyncTeleBot(TOKEN, disable_web_page_preview=True)
 
 @bot.message_handler(commands=['start', 'menu'])
 async def cmd_start(msg: types.Message):
+    markup = types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton(
+        text='Статьи',
+        callback_data='menu/articles'
+    ))
     await bot.reply_to(
         msg,
         f"Привет, @{msg.from_user.username}"
     )
 
 
-@bot.message_handler(commands=['work'])
+@bot.callback_query_handler
+async def callback_inline(query: types.CallbackQuery):
+    data = query.data
+    directory = f'./{data}/'
 
 
 async def run():
