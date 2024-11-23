9ecd457323dd03ba77f27f792f22895df2196ed2
RashingPro <rashingpro@yandex.ru>
2024-11-23 15:21:02 +0300

    Small fixes

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import asyncio
 import sys
 
 import bot
-import managers
 
 
 async def main():
@@ -15,12 +14,10 @@ async def main():
 if __name__ == '__main__':
     print("Running...")
     print("__name__ is __main__")
-    run_args = sys.argv
+    run_args = sys.argv[1:]
     print(f"Run args: {run_args}")
     if '-dev' in run_args:
         ...
     elif '-bot' in run_args:
         print("Running main()...")
         asyncio.run(main())
-
-
