3e26163a82a82af0e3cd25edaae37757fa24268b
RashingPro <rashingpro@yandex.ru>
2024-11-23 15:20:45 +0300

    Update menu feature, small fixes

--- bot.py
+++ bot.py
@@ -10,27 +10,42 @@ with open('./config/config.yml', 'r') as f:
     cfg_main = yaml.safe_load(f)
 
 TOKEN = cfg_main['bots']['main']['token']
-bot = AsyncTeleBot(TOKEN, disable_web_page_preview=True)
+bot = AsyncTeleBot(TOKEN, disable_web_page_preview=True, colorful_logs=True, parse_mode='markdown')
 
 
 @bot.message_handler(commands=['start', 'menu'])
 async def cmd_start(msg: types.Message):
-    markup = types.InlineKeyboardMarkup()
-    markup.add(types.InlineKeyboardButton(
-        text='Статьи',
-        callback_data='menu/articles'
-    ))
-    await bot.reply_to(
-        msg,
-        f"Привет, @{msg.from_user.username}",
-        reply_markup=markup
-    )
+    await MenuNavigationManager.handle_menu('menu', bot, msg, msg.from_user)
 
 
 @bot.callback_query_handler(func=lambda call: True)
 async def callback_inline(query: types.CallbackQuery):
     data = query.data
-    await MenuNavigationManager.handle_menu(data, bot, query.message, query.from_user)
+    path = data.split('?')[0]
+    args = None
+    try:
+        args = data.split('?')[1]
+    except Exception: pass
+    else:
+        args = args.split(';')
+        parsed_args = {}
+        for arg in args:
+            parsed_args[arg.split('=')[0]] = arg.split('=')[1]
+    if args is None:
+        await MenuNavigationManager.handle_menu(
+            data,
+            bot,
+            query.message,
+            query.from_user
+        )
+    else:
+        await MenuNavigationManager.handle_menu(
+            path,
+            bot,
+            query.message,
+            query.from_user,
+            **parsed_args
+        )
 
 
 async def run():
